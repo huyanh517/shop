7e820e1366517a076351c20d83effb6881260d82
anh-nguyenquanghuy <anh.nguyenquanghuy@emeal.ntttdata.com>
2023-10-16 18:19:46 +0700

    1

--- app.js
+++ app.js
@@ -10,20 +10,29 @@ $.ajax({
 function displayData(data) {
     $(data).each((index, item) => {
         let tr = "<tr>";
-        tr    += "<td scope='row'>" + Number(index + 1) + "</td>";
-        tr    += "<td>" + item.title + "</td>";
-        tr    += "<td>" + item.category + "</td>";
-        tr    += "<td>" + "$" + item.price + "</td>";
-        tr    += "</tr";
+        tr += "<td scope='row'>" + Number(index + 1) + "</td>";
+        tr += "<td>" + item.title + "</td>";
+        tr += "<td>" + item.category + "</td>";
+        tr += "<td>" + "$" + item.price + "</td>";
+        tr += "<td class='text-center '>" + "<button onclick='loadSingleProduct(" + item.id + ")' class='btn btn-warning btn-edit' data-bs-toggle='modal' data-bs-target='#productModal'>" + "Edit" +
+            "</button>" + "<button class='btn btn-danger btn-delete'>" + "Delete" + "</td>";
+        tr += "</tr";
 
         $(".tbody-products").append(tr);
     })
 }
 
-$.ajax({
-    type: "DELETE",
-    url: "https://shop-n7rx.onrender.com/users/11",
-    success: function (response) {
-        alert("Ok")
-    }
-});
+function loadSingleProduct(id) {
+    $.ajax({
+        type: "GET",
+        url: "https://shop-n7rx.onrender.com/products/" + id,
+        success: function (response) {
+            console.log(response)
+        }
+    });
+}
+
+
+function displayProductModal() {
+
+}
