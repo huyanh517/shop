f794add01ba5476f81d90ad74f27bfebb864c5c5
anh-nguyenquanghuy <anh.nguyenquanghuy@emeal.ntttdata.com>
2023-10-17 17:55:33 +0700

    1

--- shop.js
+++ shop.js
@@ -2,8 +2,25 @@ $.ajax({
     type: "GET",
     url: "https://shop-n7rx.onrender.com/products",
     success: function (response) {
-        console.log(typeof(response[28].images) == 'object')
-        
+        const featuredProduct = response.filter(item => item.rating > 4.8);
+        console.log(featuredProduct)
+        renderFeaturedProduct(featuredProduct)
     }
 });
 
+function renderFeaturedProduct(data) {
+    $(data).each((index, item) => {
+        let productCard = "<div class='col-lg-4 col-md-6 col-12 mt-4'>"
+        productCard += "<div class='card h-100'>"
+        productCard += "<img src='" + item.images[0] + "' class='card-img-top' alt='" + item.title + "'/>"
+        productCard += "<div class='card-body'>"
+        productCard += "<h5 class='card-title'>" + item.title + "</h5>"
+        productCard += "<p class='card-text'>" + item.description + "</p>"
+        productCard += "<a href='./product.html?id=" + item.id + "' class='btn btn-primary mt-3'>Detail</a>"
+        productCard += "</div>"
+        productCard += "</div>"
+        productCard += "</div>"
+
+        $(".featuredPrdBox").append(productCard)
+    })
+}
